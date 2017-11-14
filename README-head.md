2f9df3eb59a0ca04d27aa3a474b06e212e3f8b12
Peter Brightwell <peter.brightwell@bbc.co.uk>
2017-11-14 15:57:45 +0000

    Master docs now on main README page. Trailing slashes avoid redirect.

--- README-head.md
+++ README-head.md
@@ -6,6 +6,4 @@ It has been developed by the [Advanced Media Workflow Association](https://www.a
 
 See [here](https://amwa-tv.github.io/nmos) for an overview of NMOS specifcations.
 
-The formal IS-04 specification is provided at in [this GitHub repository](https://github.com/AMWA-TV/nmos-discovery-registration). These pages provide supporting documentation and HTML renders of the APIs (which are specified in RAML and JSON Schema).
-
-
+The formal IS-04 specification is provided at in [this GitHub repository](https://github.com/AMWA-TV/nmos-discovery-registration/). These pages provide supporting documentation and HTML renders of the APIs (which are specified in RAML and JSON Schema).  The links below are for the master branch of the repo with links to other branches and tags (including releases) at the end of this page.
--- make-readmes.sh
+++ make-readmes.sh
@@ -2,79 +2,58 @@
 
 # TODO: Move some of the common code into functions (DRY)
 
-HEAD=README-head.md
-CONTENTS=README-contents.md
+# Filename for all READMEs
 README=README.md
 
-echo "Making top level README.md"
-
-echo "## Branches" > $CONTENTS
-for dir in branches/*; do
-    branch=${dir##*/}
-    echo -e "\n[$branch](branches/$branch)" >>  $CONTENTS
-done
-echo >> $CONTENTS
-
-echo "## Tags" >> $CONTENTS
-for dir in tags/*; do
-    tag=${dir##*/}
-    echo -e "\n[$tag](tags/$tag)" >>  $CONTENTS
-done
-echo >> $CONTENTS
-
-
-cat $HEAD $CONTENTS > $README
-
 for b_or_t in branches tags; do
-    echo "Processing $b_or_t..."
+    echo "Processing $b_or_t $README..."
     cd $b_or_t
     for dir in */; do
         dirname=${dir%%/}
-        README=README.md
         echo "Making $dirname/$README"
         cd $dir
-            echo -e "# Documentation for $dirname\n" > $README
+            echo -e "# Documentation for $dirname\n" > "$README"
             for doc in docs/[1-9]*.md; do
                 no_ext=${doc%%.md}
                 # Top level documents have numbers ending in '.0'
                 match_top_level='^docs/[1-9][0-9]*\.0\. '
                 if [[ $doc =~ $match_top_level ]]; then
-                    linktext=${no_ext#* } 
-                    echo " - [$linktext]($doc)" >> $README
+                    linktext=${no_ext#* }
+                    echo " - [$linktext]($doc)" >> "$README"
                 else
                     # Removing the top-level part of lower-level link texts
                     # that is the part up to the hyphen and following space
                     linktext=${no_ext#* - }
-                    echo "   - [$linktext]($doc)" >> $README
+                    echo "   - [$linktext]($doc)" >> "$README"
                 fi
             done
 
-            README_APIS=html-APIs/README.md
-            echo -e "\n## APIs" >> $README
-            echo -e "# APIs for $dirname\n" > $README_APIS
+            README_APIS="html-APIs/$README"
+            echo -e "\n## APIs" >> "$README"
+            echo -e "# APIs for $dirname\n" > "$README_APIS"
             for api in html-APIs/*.html; do
                 no_ext=${api%%.html}
                 linktext=${no_ext##*/}
-                echo " - [$linktext]($api)" >> $README
-                echo " - [$linktext]($api)" >> $README_APIS
+                echo " - [$linktext]($api)" >> "$README"
+                echo " - [$linktext]($api)" >> "$README_APIS"
             done
 
-            README_SCHEMAS=html-APIs/schemas/README.md
-            echo -e "\n### [JSON Schemas](html-APIs/schemas/)" >> $README
-            echo -e "## JSON Schemas\n" > $README_SCHEMAS
+            README_SCHEMAS="html-APIs/schemas/$README"
+            echo -e "\n### [JSON Schemas](html-APIs/schemas/)" >> "$README"
+            echo -e "## JSON Schemas\n" > "$README_SCHEMAS"
             for schema in html-APIs/schemas/*.json; do
                 no_ext=${schema%%.json}
                 linktext=${no_ext##*/}
-                echo " - [$linktext](${schema##*/})" >> $README_SCHEMAS
+                echo " - [$linktext](${schema##*/})" >> "$README_SCHEMAS"
             done
 
-            README_EXAMPLES=examples/README.md
-            echo -e "\n### [Examples](examples/)" >> $README
-            echo -e "## Examples\n" > $README_EXAMPLES
+            README_EXAMPLES="examples/$README"
+            echo -e "\n### [Examples](examples/)" >> "$README"
+            echo -e "## Examples\n" > "$README_EXAMPLES"
             for example in examples/*.json; do
                 no_ext=${example%%.json}
                 linktext=${no_ext##*/}
-                echo " - [$linktext](${example##*/})" >> $README_EXAMPLES
+                echo " - [$linktext](${example##*/})" >> "$README_EXAMPLES"
             done
 
             cd ..
@@ -82,3 +61,27 @@ for b_or_t in branches tags; do
     cd ..
 done
 
+HEAD=README-head.md
+CONTENTS=README-contents.md
+
+echo "Making top level $README"
+
+echo "Adding in contents of master $README"
+# Shameful but effective - correct the links while copying text
+sed 's:(:(branches/master/:' branches/master/$README > "$CONTENTS"
+
+echo -e "\n## Branches" >> "$CONTENTS"
+for dir in branches/*; do
+    branch=${dir##*/}
+    echo -e "\n[$branch](branches/$branch/)" >>  "$CONTENTS"
+done
+
+echo -e "\n## Tags" >> "$CONTENTS"
+for dir in tags/*; do
+    tag=${dir##*/}
+    echo -e "\n[$tag](tags/$tag/)" >>  "$CONTENTS"
+done
+echo >> "$CONTENTS"
+
+
+cat "$HEAD" "$CONTENTS" > "$README"
